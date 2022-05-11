e2b6faec07baaa6dc7b2dc6d903c260acd337c66
ErminDZ <ermin@outlook.dk>
2022-05-11 14:26:27 +0200

    expanded on owner route

--- src/routes/Owners.jsx
+++ src/routes/Owners.jsx
@@ -1,7 +1,48 @@
-export default () => {
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import facade from "../apiFacade";
+
+const allOwners = () => {
+    const [owners,setOwners] = useState([]);
+    useEffect(() => {
+        facade.fetchData("owners/")
+        .then(data =>{
+            setOwners(data);
+        })
+    }, []);
+
+   
+
+    const [query, setQuery] = useState("");
     return (
+        
         <main style={{ padding: "1rem 0" }}>
-            <h2>Owners</h2>
+            
+            <div>
+                <h2>Owners</h2>
+                {/* <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>name</th>
+                            <th>address</th>
+                            <th>phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            owners.map((Owner)=>(
+                                <tr>
+                                    <td>{Owner}</td>
+                                    <td>{}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table> */}
+            </div>
         </main>
+        
     );
 }
+
+export default allOwners;
